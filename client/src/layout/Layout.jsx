b6fb9574ad285f298d88d54526efc0c199bd19ff
guiss26 <guissella584@gmail.com>
2026-04-27 21:16:25 -0800

    refactor: rename Header.jsx to Navbar.jsx

--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
-import Header from "../components/Header";
+import Navbar from "../components/Navbar";
 
 export default function Layout() {
     return (
         <>
-        <Header />
+        <Navbar />
         <Outlet />
         <Footer />
         </>
