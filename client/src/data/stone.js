e4548b88eedb8b949c687e6cbb511de2ebc86d9c
guiss26 <guissella584@gmail.com>
2026-05-12 21:41:13 +0200

    feat(data): configure stone data schema

---
+++ client/src/data/stone.js
@@ -0,0 +1,63 @@
+
+export const stone = [
+    {
+        id: 1, 
+        name: "Turquesa",
+        description: "Piedra azul verdosa, símbolo de protección y buena suierte",
+        color: "",
+        colorCode: "#5BCFCF",
+        price: 15.00,
+        image: "https://unsplash.com/es/fotos/fragmento-de-piedra-azul-y-verde-2wrAmPvCwFE",
+        avaible: true
+    },
+    {
+        id: 2,
+        name: "Amatista",
+        description: "Cuarzo violeta, transmite calma y serenidad",
+        color: "Violeta",
+        colorCode: "#9B72CF",
+        price: 12.00,
+        image: "https://unsplash.com/es/fotos/cristal-purpura-sobre-mesa-blanca-ToDanUwG4vs",
+        avaible: true
+    },
+    {
+        id: 3,
+        name: "Labradorita",
+        description: "Piedra iridiscente con destellos azules y dorados",
+        color: "Gris iridiscente",
+        colorCode: "#7A9BB5",
+        price: 18.00,
+        image: "https://unsplash.com/es/fotos/accesorio-ovalado-azul-y-negro-sobre-superficie-blanca-kHrrHMMG-ME",
+        avaible: true
+    },
+    {
+        id: 4,
+        name: "Cuarzo rosa",
+        description: "Piedra del amor y la ternura, suave y delicada",
+        color: "Rosa suave",
+        colorCode: "#F4B8C1",
+        price: 10.00,
+        image: "https://unsplash.com/es/fotos/una-piedra-preciosa-de-cuarzo-rosa-suave-y-rosa-F0WPL292Cdg",
+        avaible: true
+    },
+    {
+        id: 5,
+        name: "lodolita ",
+        description: "Tipo de cuarzo cristal que parece portar pequeños mundos. Paisajes debidos a las inclusiones de restos de roca y otros minerales que guarda en su interior",
+        color: "Negro",
+        colorCode: "#2C2C2C",
+        price: 11.00,
+        image: "https://www.google.com/search?sca_esv=ae7b427e81070a13&rlz=1C1CHBF_esES881ES881&sxsrf=ANbL-n6XFKhZ6oYycYIF46eKlrxP3BArwA:1778614148876&udm=2&fbs=ADc_l-bpk8W4E-qsVlOvbGJcDwpn60DczFdcvPnuv8WQohHLTQJsEgZIYkjbgWUViY9fymDXzEJcyFKhH_lCR1_gOJpzdm6ThUGLrTvskwyqU5F1ELEtz-j79GZezjYg3fbSU7C_9NubUqfpq_ofzHBSKUgkjM7tGBVcsQK4WbHjRyU1MORXce0gqI6qVsqVVrDo58JVO3LZ3fqmlUROZ40jvDp04R8Miw&q=lodolita&sa=X&sqi=2&ved=2ahUKEwjD4aSEvrSUAxVUUaQEHY_SBZsQtKgLegQIFhAB&biw=1242&bih=545&dpr=1.1#sv=CAMSVhoyKhBlLXBGbUxtTWgtSGtfNDhNMg5wRm1MbU1oLUhrXzQ4TToOMkgzdkI5V0VMa0xOdk0gBCocCgZtb3NhaWMSEGUtcEZtTG1NaC1Ia180OE0YADABGAcg1NGx6QhKCBABGAEgASgB",
+        avaible: true
+    }, 
+    {
+        id: 6,
+        name: "Obsidiana",
+        description: "Piedra volcánica negra protectora y elegante",
+        color: "Negro",
+        colorCode: "#2C2C2C",
+        price: 11.00,
+        image: "https://unsplash.com/es/fotos/un-objeto-negro-sentado-encima-de-un-suelo-blanco-Wu12UJK8QeE",
+        avaible: true
+    }
+]
