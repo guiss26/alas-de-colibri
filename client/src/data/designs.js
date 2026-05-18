6f44db71a2d3ec0f22ad7ef5b1fd18ac65244fa4
guiss26 <guissella584@gmail.com>
2026-05-18 21:55:51 +0200

    feat(data): configure stone data schema

---
+++ client/src/data/designs.js
@@ -0,0 +1,57 @@
+
+export const designs = [
+    {
+        id: 1,
+        name: "Anillo solitario",
+        type: "ring",
+        description: "Diseño minimalista con la piedra como protagonista",
+        price: 25.00,
+        imagen: "https://unsplash.com/es/fotos/un-par-de-anillos-que-estan-sobre-una-mesa-d5qU27rehlk",
+        includesChain: false
+    },
+    {
+        id: 2,
+        name: "Anillo trenzado",
+        type: "ring",
+        description: "Banda de plata trenzada a mano con piedra central",
+        price: 30.00,
+        imagen: "https://unsplash.com/es/fotos/un-anillo-de-oro-blanco-con-diamantes-Fux05TApirY",
+        includesChain: false
+    },
+    {
+        id: 3,
+        name: "Pulsera ",
+        type: "bracelet",
+        description: "Hilo de plata trenzado artesanalmente con piedra central",
+        price: 20.00,
+        imagen: "https://unsplash.com/es/fotos/un-brazalete-de-plata-con-un-corazon-nMvAcGHF0iQ",
+        includesChain: false
+    },
+    {
+        id: 4,
+        name: "Colgante gota",
+        type: "pendant",
+        description: "Piedra engarzada en forma de gota, elegante y delicado",
+        price: 40.00,
+        imagen: "https://unsplash.com/es/fotos/un-collar-con-una-piedra-turquesa-colgando-de-una-cadena-de-plata-BJGh9RyuSls",
+        includesChain: false
+    },
+    {
+        id: 5,
+        name: "Pendientes",
+        type: "earrings",
+        description: "Pendientez de piedra en forma de gota",
+        price: 22.00,
+        imagen: "https://unsplash.com/es/fotos/pulsera-de-cuentas-de-oro-y-purpura-I4SU-rCPbX0",
+        includesChain: false
+    },
+    {
+        id: 6,
+        name: "Colgante espadas",
+        type: "pendant",
+        description: "Colgante de espadas enlazadas",
+        price: 30.00,
+        imagen: "https://unsplash.com/es/fotos/una-mano-sostiene-un-collar-con-colgante-de-espadas-cruzadas-Xddqbavixqg",
+        includesChain: false
+    }
+]
