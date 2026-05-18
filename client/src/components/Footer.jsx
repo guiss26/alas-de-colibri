668922aff25e8c6d5fe060ee87ce7c127bf0a83d
guiss26 <guissella584@gmail.com>
2026-05-18 20:13:04 +0200

    feat(footer): implement responsive layout

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,32 +5,32 @@ import { Link } from "react-router-dom";
 export default function Footer() {
     return (
         <>
-            <footer className="bg-almond-silk-50 pt-10 px-10 pb-7 text-neutral-500">
-                <section className="flex justify-between ">
+            <footer className="bg-almond-silk-50 pt-10 px-4 md:px-10 pb-7 text-neutral-500">
+                <section className="flex flex-col gap-5 md:flex-row justify-between">
                     <div className="flex-1 flex flex-col gap-3 px-5 ">
-                        <h3 className="text-md font-bold ">Alas de Colibrí</h3>
-                        <p className="text-sm">Bisutería artesanal hecha a mano con y amor y dedicación. Cada pieza es única como tú.</p>
+                        <h3 className="text-base font-bold ">Alas de Colibrí</h3>
+                        <p className="text-xs md:text-sm">Bisutería artesanal hecha a mano con y amor y dedicación. Cada pieza es única como tú.</p>
                     </div>
 
                     <div className="flex-1 flex flex-col gap-3 px-5">
-                        <h3 className="text-md font-bold">Enlaces</h3>
-                        <Link className="text-sm">Inicio</Link>
-                        <Link className="text-sm">Crear tu Joya</Link>
-                        <Link className="text-sm">Sobre Nosotros</Link>
-                        <Link className="text-sm">Contacto</Link>
+                        <h3 className="text-base font-bold">Enlaces</h3>
+                        <Link className="text-xs md:text-sm">Inicio</Link>
+                        <Link className="text-xs md:text-sm">Crear tu Joya</Link>
+                        <Link className="text-xs md:text-sm">Sobre Nosotros</Link>
+                        <Link className="text-xs md:text-sm">Contacto</Link>
                     </div>
 
                     <div className="flex-1 flex flex-col gap-3 px-5">
-                        <h3 className="text-md font-bold">Contacto</h3>
+                        <h3 className="text-base font-bold">Contacto</h3>
 
-                        <div className="flex items-center gap-4">
-                            <p className="flex items-center gap-1 text-sm"><LuMail /> anaferm79@gmail.com</p>
-                            <Link to="https://www.instagram.com/alasdecolibri_l.a/" className="flex items-center gap-1 text-sm"><FaInstagram />alasdecolibri_l.a</Link>
+                        <div className="flex flex-col gap-2 md:flex-row md:items-center md:gap-4">
+                            <p className="text-xs flex items-center gap-1 md:text-sm"><LuMail /> anaferm79@gmail.com</p>
+                            <Link to="https://www.instagram.com/alasdecolibri_l.a/" className="text-xs flex items-center gap-1 md:text-sm"><FaInstagram />alasdecolibri_l.a</Link>
                             {/* a lo mejor poner uno debajo de otro */}
                         </div>
 
                         {/* <div> a lo mejor poner las tarjetas que se aceptan
-                            <p><FaInstagram /></p>
+                            
                         </div> */}
                     </div>
                 </section>
