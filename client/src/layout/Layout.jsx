2bd41416245d847d1e6d6c6b1117896f3ca3e5c4
guiss26 <guissella584@gmail.com>
2026-05-25 20:32:26 +0200

    style(layout): apply background styling to Layout

--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -4,7 +4,8 @@ import Navbar from "../components/Navbar";
 
 export default function Layout() {
     return (
-        <div className="flex flex-col min-h-screen bg-neutral-50">
+        // El div tiene un bg-neutral- 50
+        <div className="flex flex-col min-h-screen bg-fondo"> 
             <Navbar />
             <main className="flex-1">
                 <Outlet />
