dbd08ff8f6aa593f4c7ef6c6d2d4d80506c99225
guiss26 <guissella584@gmail.com>
2026-05-26 22:07:35 +0200

    style(navbar): update background and border colors

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ export default function Navbar() {
     return (
         <>
             {/* el header lo tenía a bg-almond-silk-50  */}
-            <header className="bg-blanco sticky top-0 z-50 backdrop-blur-sm border-b border-gray-100 shadow-sm">
+            <header className="bg-fondo-nav sticky top-0 z-50 backdrop-blur-sm border-b border-gray-300 shadow-sm">
                 <div className="flex items-center justify-between py-3 px-4">
                     {/* logo */}
                     <div className="">
